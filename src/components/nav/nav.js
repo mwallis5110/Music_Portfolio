1f96e80d56c4e3e4a5e0550bae70b8675736ed3f
Mason <mwallis5110@gmail.com>
2021-09-17 10:24:40 -0500

    Carousel structure

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from 'react-bootstrap/Nav';
-import styles from "./nav.css";
+import "./nav.css";
 
 export default function PageNav() {
   return (
--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -2,7 +2,7 @@ import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import Mixing from '../../assets/mixing.png';
 import Recording from "../../assets/recording.png";
-import General from "../../assets/general.png";
+import General from "../../assets/general2.png";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./services.css";
@@ -10,42 +10,43 @@ import "./services.css";
 export default function Services() {
   return (
     <div className="offerings">
-      <Carousel>
+      <h1>Services</h1>
+      <Carousel className = 'servicesCarousel'>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
+          <img 
+            // className="d-block w-100"
             src={Mixing} 
             //src="holder.js/800x400?text=First slide&bg=373940"
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3>Mixing Services</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src={Recording}
+            // className="d-block w-100"
+            src={General}
             // src="holder.js/800x400?text=Second slide&bg=282c34"
             alt="Second slide"
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3>Vocal Recording</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            // className="d-block w-100"
             src={General}
             //src="holder.js/800x400?text=Third slide&bg=20232a"
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>Other Services</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
